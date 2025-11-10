e8bc5209be8bbf0f6d624a6361d9970a18bc8067
fjgarcial <fjgarcial@educa.jcyl.es>
2025-11-10 11:38:40 +0100

    Delete data/utils/db_utils.py

--- data/utils/db_utils.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def obtener_datos_alumno(db_path, nombre):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM alumnos WHERE nombre = ?', (nombre,))
-    datos = cursor.fetchone()
-    conn.close()
-    return datos
