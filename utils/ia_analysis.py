86074876c175b0558af11f48315e2e7370aa0ff8
fjgarcial <fjgarcial@educa.jcyl.es>
2025-11-10 11:39:31 +0100

    Add files via upload

---
+++ utils/ia_analysis.py
@@ -0,0 +1,9 @@
+# Simulación de análisis con IA
+def analizar_texto(texto):
+    return {
+        'palabras': len(texto.split()),
+        'estructura': 'Narrativa',
+        'vocabulario': 'Común',
+        'tiempos': 'Presente',
+        'complejidad': 'Moderada'
+    }
