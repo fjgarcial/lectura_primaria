4fc1236b4253587745b0fc48d3805a6859119533
fjgarcial <fjgarcial@educa.jcyl.es>
2025-11-11 01:14:11 +0100

    Delete utils/ia_analysis.py

--- utils/ia_analysis.py
+++
@@ -1,9 +0,0 @@
-# Simulación de análisis con IA
-def analizar_texto(texto):
-    return {
-        'palabras': len(texto.split()),
-        'estructura': 'Narrativa',
-        'vocabulario': 'Común',
-        'tiempos': 'Presente',
-        'complejidad': 'Moderada'
-    }
